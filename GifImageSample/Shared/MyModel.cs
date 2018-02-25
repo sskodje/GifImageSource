49dc3c95175bbac2924aceda6dd334f70b4022c3
Sverre <sskodje@gmail.com>
2018-02-25 18:57:34 +0100

    added links to sample images for supported formats

--- GifImageSample/Shared/MyModel.cs
+++ GifImageSample/Shared/MyModel.cs
@@ -48,6 +48,9 @@ namespace GifImageSample
             items.Add(new MyModel(new Uri("http://i.imgur.com/YHoBqLR.gif")));
             items.Add(new MyModel(new Uri("https://uploads.disquscdn.com/images/c6aed8d2d4ba77c60eee3f761004fc879c8ad0727419a7dd566081a8dad2fb34.gif?w=200")));
             items.Add(new MyModel(new Uri("http://this.is.not.a.valid.uri.com")));
+            items.Add(new MyModel(new Uri("https://i.redd.it/eyvag1hrc0i01.png")));
+            items.Add(new MyModel(new Uri("https://www.w3.org/People/mimasa/test/imgformat/img/w3c_home.bmp")));
+            items.Add(new MyModel(new Uri("https://homepages.cae.wisc.edu/~ece533/images/cameraman.tif")));
             items.Add(new MyModel(new Uri("ms-appx:///this/path/does/not/exist.gif")));
             return items;
         }
